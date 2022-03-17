a20d61f3c09753990ae79b090b2ebfef4fa2c0a0
Kyungyun Lee, IOS dev <leeky16498@gmail.com>
2022-03-17 19:37:57 +0000

    Initial Commit

---
+++ JungdaeriSwift/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  JungdaeriSwift
+//
+//  Created by Kyungyun Lee on 17/03/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ JungdaeriSwift/JungdaeriSwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  JungdaeriSwiftApp.swift
+//  JungdaeriSwift
+//
+//  Created by Kyungyun Lee on 17/03/2022.
+//
+
+import SwiftUI
+
+@main
+struct JungdaeriSwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
